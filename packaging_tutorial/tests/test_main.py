42f5cefa70372ca770bf43a921c710cef1d2c1b0
FanFan-94 <fanfandouble@gmail.com>
2023-10-19 14:53:30 +0200

    test

--- packaging_tutorial/tests/test_main.py
+++ packaging_tutorial/tests/test_main.py
@@ -1,9 +1,5 @@
 import pytest
-<<<<<<< HEAD
 from add import add_value
-=======
-
->>>>>>> f16bd2b75a7e7e00db02d7a1885a9fab7c4407f7
 @pytest.mark.parametrize("test_input,expected", [(3,4), (4,5)])
 def test_main(test_input, expected):
     assert(add_value(test_input) == expected)
