f16bd2b75a7e7e00db02d7a1885a9fab7c4407f7
FanFan-94 <fanfandouble@gmail.com>
2023-10-19 14:47:40 +0200

    Update test_main.py

--- packaging_tutorial/tests/test_main.py
+++ packaging_tutorial/tests/test_main.py
@@ -1,6 +1,6 @@
 import pytest
 
-@pytest.mark.parametrize("test_input,expected", [(3,4), (4,8)])
+@pytest.mark.parametrize("test_input,expected", [(3,4), (4,5)])
 def test_main(test_input, expected):
     assert(test_input+1 == expected)
 
